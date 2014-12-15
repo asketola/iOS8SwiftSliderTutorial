7891d90ef6af4aafdf1f5a9bb099d228b92a62a9
asketola <star7anne@yahoo.com>
2014-12-14 22:32:43 -0800

    Done
    Slider Tutorial

--- iOS8SwiftSliderTutorial/ViewController.swift
+++ iOS8SwiftSliderTutorial/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        var currentValue = Int(sender.value)
+        sliderLabel.text = "\(currentValue)"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
